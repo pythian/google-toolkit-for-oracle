329f66a71eecd91d49f8336a7db6103c6f65a6e5
Gorjan Todorovski <todorovski@pythian.com>
2025-03-18 16:16:33 +0100

    Fixing the way we locate and require Oracle home

--- cleanup-oracle.sh
+++ cleanup-oracle.sh
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Set calling script name for validation tasks
+export CALLING_SCRIPT="cleanup-oracle.sh"
+
 echo Command used:
 echo "$0 $@"
 echo
